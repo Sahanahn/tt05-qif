1a20024ef25a2a8a9d3ffd25658109d6d4fff8f5
Sahanahn <131826111+Sahanahn@users.noreply.github.com>
2023-11-06 12:30:34 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -19,17 +19,27 @@ module tt_um_tb ();
     // wire up the inputs and outputs
  input reg clk;               // Clock signal
  input reg rst_n;               // Reset signal
- input reg [7:0] B;     // Input B 
- output wire [7:0] V;              // Output voltage
- output wire spike_out;            // Spike output
-
+    reg [7:0] ui_in;
+    reg[7:0] uio_in;
+    reg ena;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
     tt_um_QIFNeuron qif_neuron (
-    .clk(clk),
-        .rst_n(rst_n),
-    .B(B),
-    .V(V),
-    .spike_out(spike_out)
-  );
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
 
 endmodule
