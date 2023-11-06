f809041261ccb1d3c6458ecc9b6348535a0598c0
Sahanahn <131826111+Sahanahn@users.noreply.github.com>
2023-11-05 16:30:42 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -17,31 +17,19 @@ module tb ();
     end
 
     // wire up the inputs and outputs
-    reg  clk;
-    reg  rst_n;
-    reg  ena;
-    reg  [7:0] ui_in;
-    reg  [7:0] uio_in;
+  reg clk = 0;               // Clock signal
+  reg rst = 0;               // Reset signal
+  reg [7:0] B = -8'sd20;     // Input B set to -20
+  wire [7:0] V;              // Output voltage
+  wire spike_out;            // Spike output
 
-    wire [6:0] segments = uo_out[6:0];
-    wire [7:0] uo_out;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
-    // include power ports for the Gate Level test
-    `ifdef GL_TEST
-        .VPWR( 1'b1),
-        .VGND( 1'b0),
-    `endif
-        .ui_in      (ui_in),    // Dedicated inputs
-        .uo_out     (uo_out),   // Dedicated outputs
-        .uio_in     (uio_in),   // IOs: Input path
-        .uio_out    (uio_out),  // IOs: Output path
-        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-        .ena        (ena),      // enable - goes high when design is selected
-        .clk        (clk),      // clock
-        .rst_n      (rst_n)     // not reset
-        );
+    QIFNeuron qif_neuron (
+    .clk(clk),
+    .rst(rst),
+    .B(B),
+    .V(V),
+    .spike_out(spike_out)
+  );
 
 endmodule
