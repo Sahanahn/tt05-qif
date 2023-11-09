e17754ddffacbdda0a8421f701620fbce0398aef
Sahanahn <131826111+Sahanahn@users.noreply.github.com>
2023-11-08 17:40:24 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -7,7 +7,7 @@ that can be driven / tested by the cocotb test.py
 */
 
 // testbench is controlled by test.py
-module tb();
+module tb ();
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
     initial begin
@@ -17,8 +17,8 @@ module tb();
     end
 
     // wire up the inputs and outputs
- input reg clk;               // Clock signal
- input reg rst_n;               // Reset signal
+    reg clk;               // Clock signal
+    reg rst_n;               // Reset signal
     reg [7:0] ui_in;
     reg[7:0] uio_in;
     reg ena;
