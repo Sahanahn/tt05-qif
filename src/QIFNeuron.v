7c6130356999457c527fe5a6c33e459171733b8e
Sahanahn <131826111+Sahanahn@users.noreply.github.com>
2023-11-14 18:18:01 -0800

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -3,39 +3,28 @@ module QIFNeuron (
   input wire rst_n,       // Reset input
   input wire [7:0] B,     // Input B (8-bit, signed)
   output reg [7:0] V,     // Output voltage V (8-bit, signed)
-  output reg spike_out    // Spike output
+      
 );
 
-  reg [7:0] Z1, Z2;        // Delay flip-flops
-  wire [7:0] V_reset = -8'sd20; // Set Vreset to -20
+  
+  wire [7:0] V_reset = -8'sd20; // Set signed decimal value Vreset to -20
   wire [7:0] Vpeak = 8'd50;  // Vpeak value (Set to 50)
-  wire [7:0] A = 8'd32;      // Gain A (Set to 0.25)
-
-  reg spike_out_reg;       // Spike output register
+       
+ 
 
   // V computation
   reg [7:0] V_reg; // Intermediate register for V
+ 
   always @(posedge clk or posedge rst_n) begin
     if (rst_n) begin
       V_reg <= V_reset;
-      spike_out_reg <= 1'b0; // Initialize spike_out_reg to 0 on reset
-      Z1 <= 8'b0; // Initialize Z1 on reset
-    end else if (V_reg >= Vpeak) begin
+    end else if (V_mem >= V_th) begin
       V_reg <= V_reset;
-      spike_out_reg <= 1'b1;  // Set spike_out_reg to 1 when V >= Vpeak
-      Z1 <= 8'b0;
-      Z2 <= 8'b0;
     end else begin
-      V_reg <= A * (V_reg + V_reg * V_reg + B); // Include the square term here
-      spike_out_reg <= 1'b0;  // Set spike_out_reg to 0 when V < Vpeak
-      Z1 <= B + Z2;
+      V_reg <= V_reg +  (V_reg/8)*(V_reg/8) B/4; // Integrate the synaptic current
     end
   end
 
-  
-
-  // Assign spike_out_reg to spike_out (output wire)
-  assign spike_out = spike_out_reg;
-  assign V = Z2;
+  assign V = V_reg;
 
 endmodule
