ac77695191d2fe5fc4d22aece5fa404ddf96e8b0
Sahanahn <131826111+Sahanahn@users.noreply.github.com>
2023-11-15 10:45:27 -0800

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -9,14 +9,21 @@ module QIFNeuron (
   wire [7:0] Vpeak = 8'd50;  // Vpeak value (Set to 50)
   
   // V computation
+ // V computation
   reg [7:0] V_reg; // Intermediate register for V
   always @(posedge clk or posedge rst_n) begin
     if (rst_n) begin
       V_reg <= V_reset;
-      end else if (V_reg >= Vpeak) begin
+      
+      Z1 <= 8'b0; // Initialize Z1 on reset
+    end else if (V_reg >= Vpeak) begin
       V_reg <= V_reset;
-      end else begin
+     
+      Z1 <= 8'b0;
+      Z2 <= 8'b0;
+    end else begin
        V_reg <= V_reg + ((V_reg / 8) * (V_reg / 8)) * (B / 4);
+        
       Z2 <= V_reg;
     end
   end
